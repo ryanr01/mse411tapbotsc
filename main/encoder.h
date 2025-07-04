904760247a8f8d7a81cb5edfadbe75f531507373
AndreiPTent <84584918+AndreiPTent@users.noreply.github.com>
2025-07-04 12:14:55 -0700

    Update encoder.h

--- main/encoder.h
+++ main/encoder.h
@@ -5,5 +5,6 @@
 float encoder_get_distance_mm(void);
 void encoder_init(gpio_num_t pin_a, gpio_num_t pin_b);
 int encoder_get_position(void);
+void encoder_reset_position(void);
 
 #endif // ENCODER_H
