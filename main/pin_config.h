a306e17088e92ff865f689214e27110e3f9ed025
Ryan Rizzo <rrizz900@gmail.com>
2025-08-06 20:41:12 -0700

    pin configs

--- main/pin_config.h
+++ main/pin_config.h
@@ -43,7 +43,7 @@
 #define BOT_END_LIMIT_GPIO        7
 
 #define TOP_STEP_MOTOR_GPIO_DIR  35
-#define TOP_STEP_MOTOR_GPIO_STEP 0
+#define TOP_STEP_MOTOR_GPIO_STEP 45
 #define TOP_END_LIMIT_GPIO       42
 
 // Sensor and push button pins
